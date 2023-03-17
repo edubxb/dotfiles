a81dde8ec1c7afa3476e58ade08dd5ac944dd4bf
Eduardo Bellido Bellido <edubxb@gmail.com>
2023-03-17 01:05:14 +0100

    Neovim with pets

--- home/private_dot_config/nvim/lua/plugins/fun.lua
+++ home/private_dot_config/nvim/lua/plugins/fun.lua
@@ -1,3 +1,15 @@
 return {
   "eandrju/cellular-automaton.nvim",
+  {
+    "giusgad/pets.nvim",
+    dependencies = {
+      "giusgad/hologram.nvim",
+      "muniftanjim/nui.nvim",
+    },
+    config = function ()
+      require("pets").setup({
+          row = 4,
+        })
+    end
+  },
 }
