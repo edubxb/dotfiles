2b36a104b3aea48a680fa0c3af16ab490eae224e
Eduardo Bellido Bellido <edubxb@gmail.com>
2023-08-26 00:54:22 +0200

    nvim-cmp use cmp.SelectBehavior.Select

--- home/private_dot_config/nvim/lua/plugins/completion.lua
+++ home/private_dot_config/nvim/lua/plugins/completion.lua
@@ -115,6 +115,8 @@ return {
           end,
         },
         mapping = cmp.mapping.preset.insert({
+          ['<C-n>'] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Select }),
+          ['<C-p>'] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Select }),
           ["<C-b>"] = cmp.mapping.scroll_docs(-4),
           ["<C-f>"] = cmp.mapping.scroll_docs(4),
           ["<C-Space>"] = cmp.mapping.complete(),
