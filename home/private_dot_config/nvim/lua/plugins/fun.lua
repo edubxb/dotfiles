cdf9f1ba6e70516364f950c090e26c1b72d75fe6
Eduardo Bellido Bellido <edubxb@gmail.com>
2023-11-11 00:19:22 +0100

    Less fun with Neovim

--- home/private_dot_config/nvim/lua/plugins/fun.lua
+++ home/private_dot_config/nvim/lua/plugins/fun.lua
@@ -5,16 +5,4 @@ return {
       "nvim-treesitter/nvim-treesitter",
     }
   },
-  {
-    "giusgad/pets.nvim",
-    dependencies = {
-      "giusgad/hologram.nvim",
-      "muniftanjim/nui.nvim",
-    },
-    config = function ()
-      require("pets").setup({
-          row = 4,
-        })
-    end
-  },
 }
