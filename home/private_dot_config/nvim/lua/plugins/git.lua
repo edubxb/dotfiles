010035ed4a4a780907a909764aac1f704141f874
Eduardo Bellido Bellido <edubxb@gmail.com>
2023-11-20 21:25:53 +0100

    Neovim: remove octo plugin

--- home/private_dot_config/nvim/lua/plugins/git.lua
+++ home/private_dot_config/nvim/lua/plugins/git.lua
@@ -81,15 +81,4 @@ return {
       },
     },
   },
-  {
-    "pwntester/octo.nvim",
-    dependencies = {
-      "nvim-lua/plenary.nvim",
-      "nvim-telescope/telescope.nvim",
-      "nvim-tree/nvim-web-devicons",
-    },
-    config = function()
-      require("octo").setup()
-    end,
-  },
 }
