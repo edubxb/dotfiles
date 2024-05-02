89137f95171132923a88d7d30435ecc76d3dd03a
Eduardo Bellido Bellido <edubxb@gmail.com>
2024-05-02 21:56:51 +0200

    neovim: reformat init.lua

--- home/private_dot_config/nvim/init.lua
+++ home/private_dot_config/nvim/init.lua
@@ -32,20 +32,20 @@ keymap_opts = {
 }
 
 local rocks_config = {
-    rocks_path = vim.fn.stdpath("data") .. "/rocks",
-    luarocks_binary = "luarocks",
+  rocks_path = vim.fn.stdpath("data") .. "/rocks",
+  luarocks_binary = "luarocks",
 }
 vim.g.rocks_nvim = rocks_config
 
 local luarocks_path = {
-    vim.fs.joinpath(rocks_config.rocks_path, "share", "lua", "5.1", "?.lua"),
-    vim.fs.joinpath(rocks_config.rocks_path, "share", "lua", "5.1", "?", "init.lua"),
+  vim.fs.joinpath(rocks_config.rocks_path, "share", "lua", "5.1", "?.lua"),
+  vim.fs.joinpath(rocks_config.rocks_path, "share", "lua", "5.1", "?", "init.lua"),
 }
 package.path = package.path .. ";" .. table.concat(luarocks_path, ";")
 
 local luarocks_cpath = {
-    vim.fs.joinpath(rocks_config.rocks_path, "lib", "lua", "5.1", "?.so"),
-    vim.fs.joinpath(rocks_config.rocks_path, "lib64", "lua", "5.1", "?.so"),
+  vim.fs.joinpath(rocks_config.rocks_path, "lib", "lua", "5.1", "?.so"),
+  vim.fs.joinpath(rocks_config.rocks_path, "lib64", "lua", "5.1", "?.so"),
 }
 package.cpath = package.cpath .. ";" .. table.concat(luarocks_cpath, ";")
 
