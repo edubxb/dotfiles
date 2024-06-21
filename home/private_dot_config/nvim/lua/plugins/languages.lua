9cc9bd01cb3a54bc77fe12b5a040bbea66c9bf66
Eduardo Bellido Bellido <edubxb@gmail.com>
2024-06-21 10:58:20 +0200

    neovim: new Telescope Terraform plugin

--- home/private_dot_config/nvim/lua/plugins/languages.lua
+++ home/private_dot_config/nvim/lua/plugins/languages.lua
@@ -1,4 +1,10 @@
 return {
   "pearofducks/ansible-vim",
   "sheerun/vim-polyglot",
+  {
+    "cappyzawa/telescope-terraform.nvim",
+    dependencies = {
+      "nvim-telescope/telescope.nvim",
+    }
+  },
 }
